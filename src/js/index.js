123f01b9a0ae404d390ca03eb103c7417624f97b
watanabeyu <lovegeunyoung@sina.com>
2017-12-27 14:29:25 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,22 @@
 import '../css/app.scss';
-import plugin from './plugin';
+import Plugin from './plugin';
 
 document.getElementById('example').innerHTML = (`
     <h1>示例</h1>
-    <div class="ex-1"></div>
+    <div class="ex-1">
+      
+    </div>
 `);
 
 document.querySelector('.ex-1').innerHTML = (`
-    <div class="imgUpload"></div>
     <code>
-        &lt;div class="imgUpload"&gt;&lt;/div&gt; </br></br>
-        let upload = new imgUpload({ </br>
-        &nbsp;&nbsp;&nbsp;&nbsp;target:document.querySelector('.imgUploadasd'),</br>
-        &nbsp;&nbsp;&nbsp;&nbsp;maxnum:6, </br>
-        &nbsp;&nbsp;&nbsp;&nbsp;maxsize:0.5,</br>
-    });</br></br>
-    upload.init();
+        window.onload=function () { <br>
+        &nbsp;&nbsp;&nbsp;&nbsp;new Plugin().init(); <br>
+        }; <br>
     </code>
 `);
 
 window.onload=function () {
-    new plugin().init();
-}
+    new Plugin().init();
+};
 
--- src/js/plugin.js
+++ src/js/plugin.js
@@ -2,9 +2,21 @@ import '../css/plugin.scss';
 
 class Plugin {
     constructor(props){
-        init();
+        this.option = props;
+        this.init()
     }
     init(){
+        //参数合理性判断
+        let allowParams = new Set(['...','...']);
+        let userParams = new Set();
+        for (let i in this.option) {
+            userParams.add(i)
+        }
+        let difference = new Set([...userParams].filter(x => !allowParams.has(x)));//计算用户的参数和允许参数的差集
+        if (difference.size > 0) {//有未定的参数键值对
+            console.warn(`'${Array.from(difference)}' are not allowed , param 'option' can only accept these configuration : '${Array.from(allowParams)}'`)
+        }
+        //效果
         document.querySelector('body').innerText = '这是插件初始化写下的一段话'
     }
 }
